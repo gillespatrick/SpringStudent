c9dcf3e170ec1924e935eca378df136e3ac85c96
gillespatrick <ngondogillespatrick@gmail.com>
2023-04-03 20:42:43 -0400

    Link BackEnd and FrontEnd with proxy. Also change UnFetch version 4.2.0

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
-import {Button,Radio} from "antd"
+import {getAllStudents} from './client';
+import {useState,useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <Button type="primary">John Doe</Button>
-        <Radio.Group >
-            <Radio.Button value="large">Large</Radio.Button>
-            <Radio.Button value="default">Default</Radio.Button>
-            <Radio.Button value="small">Small</Radio.Button>
-        </Radio.Group>
-    </div>
-  );
+    const [students, setStudents] = useState([]);
+    const fetchStudents = () =>
+        getAllStudents()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setStudents(data)
+            })
+
+    useEffect( () => {
+        console.log("Component is mounted");
+        fetchStudents()
+    }, []);
+
+    if(students <=0){
+        return "No Data"
+    }
+
+
+    return students.map((student, index) => {
+       return  <p key={index}>{student.id}- {student.name} </p>
+    })
 }
 
 export default App;
